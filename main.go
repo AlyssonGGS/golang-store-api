08a1ff36e76278fc7752a233f0391406b20e4aa5
Alysson <alyssonggs@hotmail.com>
2018-09-02 12:05:21 -0300

    Criando rota para recuperar um produto

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/alyssonggs/golang-store-api/handle"
+	"github.com/alyssonggs/golang-store-api/repository/memory"
 )
 
 func main() {
-	handler := &handle.Handle{}
+	memStorage := &memory.MemoryStorage{}
+	handler := &handle.Handle{Storage: memStorage}
 
 	http.HandleFunc("/store", handler.Hello)
+	http.HandleFunc("/store/product", handler.GetProductInfo)
 
 	http.ListenAndServe(":8080", nil)
 }
